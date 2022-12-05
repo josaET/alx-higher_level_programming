b66c9159ff71a8d7515e463d1e520afcd970e7b2
josaET <111006524+josaET@users.noreply.github.com>
2022-12-05 18:56:05 +0300

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        separator = ""
+        for col in row:
+            print("{:s}{:d}".format(separator, col), end="")
+            separator = " "
+        print()
